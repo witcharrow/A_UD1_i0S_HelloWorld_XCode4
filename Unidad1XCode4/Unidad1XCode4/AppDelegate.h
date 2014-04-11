5eb1530549c21e7aafaeaf657b88be9844520685
witcharrow <alejandro.marijuan@googlemail.com>
2014-04-11 22:29:26 +0100

    Carga inicial, falta el main

---
+++ Unidad1XCode4/Unidad1XCode4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Unidad1XCode4
+//
+//  Created by user11394 on 11/04/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
